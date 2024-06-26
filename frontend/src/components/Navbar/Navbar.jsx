a674a1017459b9276e18ce0342f4f8dd44db4485
Hasibul Hasan Nirob <hasibulnirob987@gmail.com>
2024-06-27 02:17:37 +0600

    Login, Signup page design completed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import {Link} from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("");
 
@@ -10,10 +11,11 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={assets.logo} alt='' className='logo' />
         <ul className='navbar-menu'>
-            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-            <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile App</li>
-            <li onClick={()=>setMenu("contact")} className={menu==="contact"?"active":""}>Contact</li>
+          <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
+            
+            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
+            <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile App</a>
+            <a href='#footer' onClick={()=>setMenu("contact")} className={menu==="contact"?"active":""}>Contact</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt='' />
@@ -21,7 +23,7 @@ const Navbar = () => {
                 <img src={assets.basket_icon} alt='' />
                 <div className="dot"></div>
             </div>
-            <button>Sign In</button>
+            <button onClick={()=> setShowLogin(true)}>Sign In</button>
         </div>
     </div>
   )
