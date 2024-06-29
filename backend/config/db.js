94a48c728eadac9fa5b5bf4fa4afe44399a515da
Hasibul Hasan Nirob <hasibulnirob987@gmail.com>
2024-06-29 22:03:10 +0600

    All updated

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://hasibulnirob987:userdatabase@cluster0.tdijz8w.mongodb.net/food-delivery-website').then(()=>console.log("DB Connected"))
-}
